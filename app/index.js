d092a87f9816b7b7df2833a6458944ac72889c95
damir.keldibekov <damir.keldibekov@inessoft.kz>
2017-09-11 14:38:06 +0600

    fix launch screen

--- app/index.js
+++ app/index.js
@@ -2,24 +2,24 @@
  * @flow
  */
 
-XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
-    GLOBAL.originalXMLHttpRequest :
-    GLOBAL.XMLHttpRequest;
-
-// fetch logger
-global._fetch = fetch;
-global.fetch = function (uri, options, ...args) {
-    return global._fetch(uri, options, ...args).then((response) => {
-        console.log('Fetch', { request: { uri, options, ...args }, response });
-        return response;
-    });
-};
-
-import 'core-js';
-
-window.onunhandledrejection = function(promise, reason) {
-    console.log('window.onunhandledrejection is', promise, reason);
-};
+// XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
+//     GLOBAL.originalXMLHttpRequest :
+//     GLOBAL.XMLHttpRequest;
+//
+// // fetch logger
+// global._fetch = fetch;
+// global.fetch = function (uri, options, ...args) {
+//     return global._fetch(uri, options, ...args).then((response) => {
+//         console.log('Fetch', { request: { uri, options, ...args }, response });
+//         return response;
+//     });
+// };
+//
+// import 'core-js';
+//
+// window.onunhandledrejection = function(promise, reason) {
+//     console.log('window.onunhandledrejection is', promise, reason);
+// };
 
 import React, { Component } from 'react';
 import {
